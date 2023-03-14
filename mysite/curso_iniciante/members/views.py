859aced870e4163a7b033df9582d9afe0e256928
Marcos Vinicius Silva Elias <110794069+engmarcoselias@users.noreply.github.com>
2023-03-13 21:24:44 -0300

    Curso Django-Mostrar Dados
    -Adicionando modelo mestre

--- mysite/curso_iniciante/members/views.py
+++ mysite/curso_iniciante/members/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers':mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymembers = Members.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
